d718d802ff54b7946e40eb043015b638554da40c
Mahendra Dani <danimahendra0904@gmail.com>
2024-11-08 21:59:18 +0530

    configured prisma and database

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,12 @@
 import { Hono } from 'hono'
+import { getPrisma } from '../lib/prisma'
 
 const app = new Hono<{ Bindings: CloudflareBindings }>()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get('/', async (c) => {
+  const prisma = getPrisma(c.env.DATABASE_URL)
+  const users = await prisma.user.findMany();
+  return c.json({message : "Hello World!",users});
 })
 
 export default app
